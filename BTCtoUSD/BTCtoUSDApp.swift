5086dc3364fb865f5163e9d5d7bd1e27bd968683
Jakir Hossain <jakirseu@gmail.com>
2023-10-23 17:16:49 +0600

    Initial Commit

---
+++ BTCtoUSD/BTCtoUSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCtoUSDApp.swift
+//  BTCtoUSD
+//
+//  Created by Jakir Hossain on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BTCtoUSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
